741b4fe1eb1bf473d5012b56a99a5d13a63feb76
lkepitter <lkepitter@hotmail.co.uk>
2022-07-15 10:51:43 +0100

    added fetch request to api

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const api = `https://gnews.io/api/v4/top-headlines?topic=technology&lang=en&token=${process.env.REACT_APP_APIKEY}`;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      const response = await fetch(api);
+      const data = response.json();
+      setArticles(data.articles);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">News</header>
